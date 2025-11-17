d51f423cffeb1143505f19acab9b2b57abdc3aeb
q-tran14 <tranquangquan.it@gmail.com>
2025-11-18 01:07:02 +0700

    Test 3D possible position and direction in 3D space for 3D gameplay

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Quaternion.LookRotation(Vector3.forward, Vector3.up);    // Top Face
+    // Quaternion.LookRotation(Vector3.forward, Vector3.right); // Right Face
+    // Quaternion.LookRotation(Vector3.down, Vector3.back);     // Back Face
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
